c1d473aefd40e146c6a105d958a188998becfcc4
ishaantaylor <ist2@case.edu>
2016-10-08 20:16:57 -0700

    better organized twitter.py to handle the preprocessing of the specific twittercase and stream.py to just stream
    also added nice logging

--- tweets/stream.py
+++ tweets/stream.py
@@ -1,14 +1,10 @@
 import tweepy
-import json
-import sys
 import os.path
-import cPickle as pickle
 
-from markov import Markov
 from auth import auth_twitter
 from listener import TweetListener
 
-def stream_tweets_from(location, limit=5000, words=None):
+def stream_tweets_from(location, markov, limit=5000, words=None):
 	"""
 	@param location 	longitude&latitude ex. [-122.75,36.8,-121.75,70.8]
 	@param limit    	integer
@@ -17,28 +13,8 @@ def stream_tweets_from(location, limit=5000, words=None):
 
 	auth = auth_twitter()
 
-	# load ngrams
-	try:
-		ngrams = pickle.load( open( "ngrams.p", "rb" ) )
-	except IOError:
-		ngrams = dict()
-
-	# load beginning words
-	try:
-		beginnings = pickle.load( open( "beginnings.p", "rb" ) )
-	except IOError:
-		beginnings = []
-
-	# create markov engine
-	print ngrams
-	print beginnings
-	markov = Markov(ngrams, beginnings)
-
 	# start stream
-	try:
-		tweet_listener = TweetListener(markov)
-		stream = tweepy.Stream(auth, tweet_listener)
-		stream.filter(locations=location, track=words)
-	except KeyboardInterrupt:
-		pickle.dump( markov.two_gram_follow_probability, open( "ngrams.p", "wb" ) )
-		pickle.dump( markov.beginnings, open( "beginnings.p", "wb" ) )
+	tweet_listener = TweetListener(markov)
+	stream = tweepy.Stream(auth, tweet_listener)
+	stream.filter(locations=location, track=words)
+	
--- twitter.py
+++ twitter.py
@@ -1,3 +1,10 @@
+import sys
+# TODO: instead of pickle module for data serialization, use JSON or MessagePack
+import cPickle as pickle
+import os.path
+import logging
+from tweets.markov import Markov
+
 from tweets.stream import stream_tweets_from
 
 
@@ -6,8 +13,50 @@ LIMIT = 50000
 WORDS = ['technology']
 
 def main():
-	stream_tweets_from(SAN_FRANCISCO, LIMIT, WORDS)
-	# TODO: on stream close, save ngram model (to file ?)
+	# set up logger
+	root = logging.getLogger()
+	root.setLevel(logging.DEBUG)
+
+	ch = logging.StreamHandler(sys.stdout)
+	ch.setLevel(logging.DEBUG)
+	formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+	ch.setFormatter(formatter)
+	root.addHandler(ch)
+	
+
+	# load ngrams
+	try:
+		ngrams = pickle.load( open( "ngrams.p", "rb" ) )
+		logging.info("Loaded ngrams.p")
+	except IOError:
+		ngrams = dict()
+		logging.warning("Failed to load ngrams.p.")
+
+	# load beginning words
+	try:
+		beginnings = pickle.load( open( "beginnings.p", "rb" ) )
+		logging.info("Loaded beginnings.p")
+	except IOError:
+		beginnings = []
+		logging.warning("Failed to load beginnings.p.")
+
+
+	# create markov engine
+	MARKOV = Markov(ngrams, beginnings)
+
+	try:
+		stream_tweets_from(SAN_FRANCISCO, MARKOV, LIMIT, WORDS)
+	except KeyboardInterrupt:
+		# save ngram models and beginnings
+		try:
+			pickle.dump( MARKOV.two_gram_follow_probability, open( "ngrams.p", "wb" ) )
+			pickle.dump( MARKOV.beginnings, open( "beginnings.p", "wb" ) )
+			logging.info("Saved markov model data.")
+		except e:
+			print logging.error("Failed to save markov model data."),
+			print sys.exc_info()[0]
+
+
 
 if __name__ == "__main__":
 	main()
